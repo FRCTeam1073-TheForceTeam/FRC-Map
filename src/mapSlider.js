dea603b610f413ebb93fea14cf10b4beb658d088
Errica Cheng <errica.cheng@gmail.com>
2016-12-18 14:48:26 -0500

    [INCOMPLETE] Added if statement to filter events by year
    Not currently working - only 2016wvrox seems to be affected at all by
    the code, but will only disappear when the slider is moved.

--- src/mapSlider.js
+++ src/mapSlider.js
@@ -5,9 +5,13 @@ function mapSliderEvent( frcInfo, newYear ) {
     // Retrieve the current year
     var currentYear = frcInfo.currentYear;
 
-    if (currentYear != newYear){
+    if (currentYear != newYear) {
         // Loop through the teams and events and show or hide the teams as the year goes up or down
     	console.log('Sliding from year: ' + currentYear + ' to: ' + newYear);
+    	if (event.year != currentYear) {
+        }
+        else {
+        }
     }
     
     // Save the new year value as the current year
