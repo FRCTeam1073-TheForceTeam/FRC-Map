76f763898b72479fcb8450140730e5f92f997511
Errica Cheng <errica.cheng@gmail.com>
2016-12-20 00:32:27 -0500

    Implemented map slider
    With Mr. St. Hilaire's help, we were able to properly implement the map
    slider. There is a good amount of code comments for anyone curious how
    it works.

--- src/mapSlider.js
+++ src/mapSlider.js
@@ -8,11 +8,25 @@ function mapSliderEvent( frcInfo, newYear ) {
     if (currentYear != newYear) {
         // Loop through the teams and events and show or hide the teams as the year goes up or down
     	console.log('Sliding from year: ' + currentYear + ' to: ' + newYear);
-    	if (event.year != currentYear) {
-            eventInfo.marker.setVisible(false);
-        }
-        else {
-        	eventInfo.marker.setVisible(true);
+    	
+    	// Only one year is shown at a time, so get the events for the current year first to hide them
+    	var events = frcInfo.getEventListByYear(currentYear);
+    	
+    	// Loop through the list of events for currentYear, make the marker for that event info invisible if it exists
+    	for ( i=0; i<events.length; i++ ) {
+    		eventInfo = frcInfo.getEvent(events[i]);
+    		if ( eventInfo.marker )
+    			eventInfo.marker.setVisible(false);
+    	}
+    			
+    	// Now get the events for the new year to show them
+    	var events = frcInfo.getEventListByYear(newYear);
+    			
+    	// Loop through the list of events for newYear, make the marker for that event info visible if it exists
+        for ( i=0; i<events.length; i++ ) {
+        	eventInfo = frcInfo.getEvent(events[i]);
+        	if ( eventInfo.marker )
+        		eventInfo.marker.setVisible(true)
         }
     }
     
